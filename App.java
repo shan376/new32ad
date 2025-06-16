fee0705737bcd00e328294a09c8878c76a99bede
root <root@ip-172-31-90-159.ec2.internal>
2025-06-16 10:37:19 +0000

    feat: added App.java and AppTest.java

---
+++ App.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("✅ Hello from Jenkins build! Java 17 is working correctly.");
+    }
+}
